ba8dea44483423546f97543cd878d37e1807d74c
Bernhard Hettman <bernhard.hettman@postnord.com>
2024-12-11 10:35:57 +0100

    chore: remove comments

--- 2024/day11/solution.ts
+++ 2024/day11/solution.ts
@@ -22,18 +22,15 @@ const transformStone = (stone: number): number[] => {
   return [stone * 2024];
 };
 
-// Process a single blink using Highland streams
 const blink = (stones: Highland.Stream<number>): Highland.Stream<number> =>
   stones.flatMap((stone) => _(transformStone(stone)));
 
-// Simulate multiple blinks and return the total number of stones as a Promise
 const simulateBlinks = async (stones: number[], blinks: number): Promise<number> => {
   const initialStream = _(stones);
 
-  // Apply the blink transformations `blinks` times
   const finalStream = await _(R.range(0, blinks))
-    .reduce(initialStream, (currentStream) => blink(currentStream)) // Chain the blinks
-    .flatMap((stream) => stream.reduce(0, (count) => count + 1)) // Count the number of stones
+    .reduce(initialStream, (currentStream) => blink(currentStream)) 
+    .flatMap((stream) => stream.reduce(0, (count) => count + 1))
     .toPromise(Promise);
 
   return finalStream;
